6e56d5639a1ce11d03669f2ac570e45595e0ade3
nicolasgarciacomp <nicolasgarciacomp@gmail.com>
2016-09-08 15:54:14 -0300

    Agrego trabajo procedural

---
+++ OpenMP/TP1/practico_01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 100000000
+
+int contenedor[TAM];
+int i, j, valor;
+double fin = 0;
+clock_t t_ini, t_fin;
+
+void main(void) {
+
+t_ini = clock();
+
+for(i = 0; i<TAM; i++) {
+	valor = rand() % 20;
+	contenedor[i] = valor;
+	//printf("%d\n", contenedor[i]);
+}
+
+for(i = 0; i<TAM; i++) {
+	contenedor[i] = contenedor[i] + contenedor[i-1];
+	//printf("%d\n", contenedor[i]);
+}
+
+t_fin = clock();
+fin = (double)(t_fin - t_ini) / CLOCKS_PER_SEC;
+printf("%f segundos\n", fin);
+
+}
+
