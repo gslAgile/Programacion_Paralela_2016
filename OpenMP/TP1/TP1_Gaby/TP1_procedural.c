34a208bace45f33f926cc54383ab78363784259f
gslAgile <gsosaludu@gmail.com>
2017-07-14 00:59:46 -0300

    Repasando tutorial 1 openMP

--- OpenMP/TP1/TP1_Gaby/TP1_procedural.c
+++ OpenMP/TP1/TP1_Gaby/TP1_procedural.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #define TAM 100000000
 
+/* Declaracion de variables*/
 int contenedor[TAM];
 int i, j, valor, resultado;
-double fin = 0;
-clock_t t_ini, t_fin;
+double tiempoFinal = 0, t_ini, t_fin;
 
+/* Funcion principal*/
 void main(void) {
 
+/* Inicializacion e valores*/
 resultado = 0;
 
 for(i = 0; i<TAM; i++) {
@@ -18,7 +21,9 @@ for(i = 0; i<TAM; i++) {
 	//print("%d\n", contenedor[i]);
 }
 
-t_ini = clock();
+/* Comienzo de suma de prefijo paralela*/
+
+t_ini = omp_get_wtime(); // inicio de cuenta de tiempo
 
 for(i = 0; i<TAM; i++) {
 	resultado = resultado + contenedor[i];
@@ -26,14 +31,16 @@ for(i = 0; i<TAM; i++) {
 	//printf("%d\n", contenedor[i]);
 }
 
-t_fin = clock();
-fin = (double)(t_fin - t_ini) / CLOCKS_PER_SEC;
+t_fin = t_ini = omp_get_wtime(); // fin de cuenta de tiempo
+
+tiempoFinal = (t_fin - t_ini);
+
+printf(" Tiempo de total: %.2f segundos\n\n", tiempoFinal);
+
 printf("%d\n", contenedor[999995]);
 printf("%d\n", contenedor[999996]);
 printf("%d\n", contenedor[999997]);
 printf("%d\n", contenedor[999998]);
 printf("%d\n", contenedor[999999]);
-
-printf("%f segundos\n", fin);
 
 }
--- OpenMP/tutorial_1/tarea_c.c
+++ OpenMP/tutorial_1/tarea_c.c
@@ -1,7 +1,7 @@
 /*
 *
 * c) Crea una nueva variable e inicializarla fuera de la región paralela y pasa la como privada
-* a la zona paralela, ¿El valor de la variable dentro de la región paralela es correcto? prueba usando fistprivate.
+* a la zona paralela, ¿El valor de la variable dentro de la región paralela es correcto? prueba usando firstprivate.
 *
 */
 
@@ -23,8 +23,9 @@ else
 	nthreads = 10;
 
 omp_set_num_threads(nthreads);
- 
-#pragma omp parallel private(nthreads, thread) firstprivate(nueva_var)//private(nthreads, thread, nueva_var)
+
+//#pragma omp parallel private(nthreads, thread, nueva_var) 
+#pragma omp parallel private(nthreads, thread) firstprivate(nueva_var)
   {
  
   thread = omp_get_thread_num(); // funcion de omp.h que retorna el numero id de hilo
@@ -45,7 +46,7 @@ omp_set_num_threads(nthreads);
   *  inicializado fuera de la region paralela. Ya que en esta region como cada hilo se lo asigna localmente
   *  etonces no sabe como inicializar y la deja vacia o con valor basura.
   *  Para evitar esto lo que hacemos es lograr que tome el valor de inicializacion fuera de la region parelela 
-  *  que se logra con la la sentencia 'firstprivate'.
+  *  que se logra con la sentencia 'firstprivate'.
   */
  
 }
