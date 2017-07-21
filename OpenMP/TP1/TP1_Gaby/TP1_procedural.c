7aee7db91f0f3c22eeba03c65a230c00d1ff23b1
gslAgile <gsosaludu@gmail.com>
2017-07-21 01:01:31 -0300

    Actualizo version TP1 procedural

--- OpenMP/TP1/TP1_Gaby/TP1_procedural.c
+++ OpenMP/TP1/TP1_Gaby/TP1_procedural.c
@@ -2,44 +2,85 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define TAM 100000000
+/* Definicion de Macros*/
+#define TAM 100000000 /* Tamaño maximo de vectores*/
+#define porcion 100000 /* Porcion de iteraciones*/
 
-/* Declaracion de variables*/
-int contenedor[TAM];
-int i, j, valor, resultado;
-double tiempoFinal = 0, t_ini, t_fin;
+/* Declaracion de funciones*/
+void inicializar_vector(int * vector);
+
 
 /* Funcion principal*/
-void main(void) {
+int main(void) {
 
-/* Inicializacion e valores*/
-resultado = 0;
+	/* Declaracion de variables*/
+	int contenedor[TAM];
+	int i, j, valor, resultado;
+	double tiempoProcedural = 0, tiempoCarga = 0, t_ini, t_ini0, t_fin, SpeedUps;
 
-for(i = 0; i<TAM; i++) {
-	contenedor[i] = 1;
-	//print("%d\n", contenedor[i]);
+	/* Inicializacion e valores*/
+	resultado = 0;
+
+	t_ini0 = omp_get_wtime(); // inicio de tiempo total
+
+	inicializar_vector(contenedor);
+
+	/* Comienzo de suma de prefijo paralela*/
+
+	t_ini = omp_get_wtime(); // inicio de cuenta de tiempo
+
+	for(i = 0; i<TAM; i++) {
+		resultado = resultado + contenedor[i];
+		contenedor[i] = resultado; // +=
+		//printf("%d\n", contenedor[i]);
+	}
+
+	t_fin = omp_get_wtime(); // fin de cuenta de tiempo
+
+	/* Calculos finales*/
+	tiempoCarga = (t_ini - t_ini0);
+	tiempoProcedural = (t_fin - t_ini);
+	SpeedUps = tiempoProcedural/tiempoProcedural;
+
+
+	/* Presentacion de datos*/
+	printf("Caracteristicas del sistema\n");
+	printf("Numero de hilos disponibles: \n");
+	printf("Hilos usados para la inicializacion de vector: \n");
+	printf("Hilos usados para Calculos: \n");
+
+	printf(" Tiempo de carga de vector procedural: %.2f segundos\n", tiempoCarga);
+	printf(" Tiempo de total de calculo procedural: %.2f segundos\n", tiempoProcedural);
+	printf(" Speedups procedural: %.2f \n\n", SpeedUps);
+
+	printf("%s\n", "Ultimos valores obtenidos: ");
+	printf("%d\n", contenedor[999995]);
+	printf("%d\n", contenedor[999996]);
+	printf("%d\n", contenedor[999997]);
+	printf("%d\n", contenedor[999998]);
+	printf("%d\n", contenedor[999999]);
+
+	return 0;
+
 }
 
-/* Comienzo de suma de prefijo paralela*/
+/* Implementacion de funciones*/
 
-t_ini = omp_get_wtime(); // inicio de cuenta de tiempo
 
-for(i = 0; i<TAM; i++) {
-	resultado = resultado + contenedor[i];
-	contenedor[i] = resultado; // +=
-	//printf("%d\n", contenedor[i]);
-}
+/*
+* Descripcion:
+* @param *vector: puntero del tipo int que indica la posicion de un vector
+* @return: no aplica
+*/
+void inicializar_vector(int * vector)
+{
+	int i;
+	int nthreads_max= omp_get_max_threads();
+	omp_set_num_threads(nthreads_max);
 
-t_fin = t_ini = omp_get_wtime(); // fin de cuenta de tiempo
-
-tiempoFinal = (t_fin - t_ini);
-
-printf(" Tiempo de total: %.2f segundos\n\n", tiempoFinal);
-
-printf("%d\n", contenedor[999995]);
-printf("%d\n", contenedor[999996]);
-printf("%d\n", contenedor[999997]);
-printf("%d\n", contenedor[999998]);
-printf("%d\n", contenedor[999999]);
-
+	#pragma opm parallel for private(i) schedule(auto)
+	{
+		for(i = 0; i<TAM; i++)
+			vector[i] = 1;
+	}
 }
